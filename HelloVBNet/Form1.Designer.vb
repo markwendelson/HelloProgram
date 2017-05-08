9ae3f7dbcebf0d0b09ef19d2b12aa02ae8cfeac8
markwendelson <markwendelson@gmail.com>
2017-05-08 17:12:42 +0800

    Hello Program using VB.Net
    Hello Program using VB.Net

---
+++ HelloVBNet/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.cmdHello = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Green
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
+        Me.Label1.Size = New System.Drawing.Size(448, 61)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your name:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.txtName.Location = New System.Drawing.Point(47, 87)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(340, 30)
+        Me.txtName.TabIndex = 1
+        Me.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'cmdHello
+        '
+        Me.cmdHello.Location = New System.Drawing.Point(258, 150)
+        Me.cmdHello.Name = "cmdHello"
+        Me.cmdHello.Size = New System.Drawing.Size(129, 47)
+        Me.cmdHello.TabIndex = 2
+        Me.cmdHello.Text = "Say hello"
+        Me.cmdHello.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(448, 244)
+        Me.Controls.Add(Me.cmdHello)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents cmdHello As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+End Class
---
+++ HelloVBNet/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub cmdHello_Click(sender As Object, e As EventArgs) Handles cmdHello.Click
+        If txtName.Text = "" Then
+            ErrorProvider1.SetError(txtName, "Name Required")
+            Exit Sub
+        Else
+            ErrorProvider1.SetError(txtName, "")
+        End If
+
+        MessageBox.Show("Hello " & txtName.Text & vbNewLine & "The time is: " & TimeOfDay)
+        Me.Text = "Hello " & txtName.Text
+    End Sub
+End Class
